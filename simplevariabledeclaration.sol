2e4ed8b30fd56c5e70a2f8ff8b93adca37964b16
deepakbaberwal01 <32518558+deepakbaberwal01@users.noreply.github.com>
2017-11-16 13:32:08 +0530

    simplevariabledeclaration.sol

---
+++ simplevariabledeclaration.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.18;
+contract demo{
+    string public word = "hello"; //if we dont use public then when running we cant acess the variables
+    uint128 public a = 42;
+    
+   /* function getWord() constant returns(string){
+        return word;
+        
+    }
+    */
+}
+
+    
