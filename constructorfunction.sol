6aee4161d6d56a06d23066f5064685aae0d03ed1
deepakbaberwal01 <32518558+deepakbaberwal01@users.noreply.github.com>
2017-11-23 14:52:33 +0530

    constructorfunction.sol

---
+++ constructorfunction.sol
@@ -0,0 +1,12 @@
+//this program shows example of constructor function
+
+pragma solidity ^0.4.18;
+contract Coursetro{
+    string public fname;
+    uint public age;
+    
+    function demo() public{ //demo is constructor function
+        fname = "deepk";
+        age = 28;
+    }
+}
